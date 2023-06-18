b0540d2a221a26b55068f9af5f53651f249c360a
Anas <anaselnahef12@gmail.com>
2023-06-18 21:15:33 +0300

    add the media page implemenation

--- pages/profile/[username]/media.vue
+++ pages/profile/[username]/media.vue
@@ -1,5 +1,45 @@
 <template>
-    <div>media</div>
+    <div>
+        <MainSection withoutHeader profile :title="user.name" :tweetsCount="tweetsCount">
+            <ProfileHeader />
+            <ProfileTabs />
+
+            <div v-if="tweetsLoading" class="flex justify-center items-center h-24">
+                <UISpinner />
+            </div>
+            <TweetListFeed v-else :tweets="tweets" home />
+        </MainSection>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+definePageMeta({ middleware: ["auth"] });
+const { user } = useAuth();
+useHead({
+    title: `(@${user.value.username}) / Twitter ${user.value.name}`,
+});
+
+const username = useRoute().params.username;
+const tweets = ref([]);
+const tweetsLoading = ref(true);
+const tweetsCount = ref("");
+
+const { getUserTweets } = useTweets();
+
+onBeforeMount(async () => {
+    getUserTweetsFun();
+});
+
+const getUserTweetsFun = async () => {
+    try {
+        tweetsLoading.value = true;
+        const data = await getUserTweets(username);
+        tweets.value = data.tweets.filter((tweet) => tweet.media.length > 0);
+        tweetsCount.value = data.mediaCount;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        tweetsLoading.value = false;
+    }
+};
+</script>
