101992c3f529ffb6f235f4763a0b2b3c1489ba24
Anas <anaselnahef12@gmail.com>
2024-03-05 22:41:25 +0200

    change

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,4 +1,5 @@
 export default defineNuxtRouteMiddleware(async () => {
     const user = useUser();
+    console.log("user", user);
     if (!user.value) return navigateTo("/login", { replace: true });
 });
